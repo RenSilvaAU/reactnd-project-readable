6483dd18c06e21fba1f87d820c76f5a57e5b802f
Ren <renato_s_silva@hotmail.com>
2017-10-21 11:18:34 +1100

    scafolding

---
+++ frontend/src/readable/Category.js
@@ -0,0 +1,16 @@
+class Category extends Component {
+
+	// identical to the default view, but filtered to only include posts with the selected category
+
+
+  render() {
+    return (
+      <div >
+      	empty
+      </div>
+    );
+  }
+}
+
+export default Category;
+
