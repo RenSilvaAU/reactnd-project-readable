230b8988eec1c7ab6b12deba3b365be6e8aed746
Ren Silva <renato_s_silva@hotmail.com>
2017-10-25 00:08:17 +1100

    first dispatcher sort of working

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,7 +17,7 @@ import { Provider } from 'react-redux'
 
 
 const store = createStore(
- // reducer,
+  reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
